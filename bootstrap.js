5677043233419b1d6e0ba51e683e272043e53236
Tasneem Natshah <t.natshah@gmail.com>
2023-06-14 11:37:31 +0300

    Add some other HTML formats for the report #4

---
+++ bootstrap.js
@@ -0,0 +1,21 @@
+var reporter = require('cucumber-html-reporter');
+
+var options = {
+  theme: 'bootstrap',
+  jsonFile: 'tests/reports/index.json',
+  output: 'tests/reports/index.html',
+  reportSuiteAsScenarios: true,
+  scenarioTimestamp: true,
+  launchReport: true,
+  metadata: {
+    "App Version": "7.1.0",
+    "Test Environment": "development",
+    "Browser": "Google Chrome",
+    "Platform": "Linux",
+    "Parallel": "Scenarios",
+    "Executed": "Remote"
+  },
+  failedSummaryReport: true,
+};
+
+reporter.generate(options);
